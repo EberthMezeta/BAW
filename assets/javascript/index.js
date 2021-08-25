72f5b8b3453e01929d69ce1288aa141059f2798a
Eberth <the.jonkey@gmail.com>
2021-08-25 18:58:16 -0500

    se agregó formato al javascript

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -4,35 +4,36 @@ const containerArticles = document.getElementById("articles-container");
 const categorySelect = document.getElementById("Category");
 
 const validateBarSearch = () => {
-    const valueInput = searchInput.value;
-    if (valueInput.trim() !== "") {
-        return true;
-    }
-    return false;    
-}
+  const valueInput = searchInput.value;
+  if (valueInput.trim() !== "") {
+    return true;
+  }
+  return false;
+};
 
-const  getResponse = async () =>{
-    try {
-        let Search = searchInput.value;
-        let Category = categorySelect.value;
-        const res = await fetch(`http://localhost/BAW/services/service.php?q=${Search}&c=${Category}`,{
-            method : 'GET',
-            headers: {
-                'Accept-Language': window.navigator.language.toString()
-            }
-        });
-        containerArticles.innerHTML = await res.text();
-    } catch (error) {
-        console.log(error);
-    }
-}
+const getResponse = async () => {
+  try {
+    let Search = searchInput.value;
+    let Category = categorySelect.value;
+    const res = await fetch(
+      `http://localhost/BAW/services/service.php?q=${Search}&c=${Category}`,
+      {
+        method: "GET",
+        headers: {
+          "Accept-Language": window.navigator.language.toString(),
+        },
+      }
+    );
+    containerArticles.innerHTML = await res.text();
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-searchButton.addEventListener("click",()=>{
-    
-    if(validateBarSearch()){
-        getResponse();
-        return;
-    }
-    alert("Barra de busqueda vacía");
+searchButton.addEventListener("click", () => {
+  if (validateBarSearch()) {
+    getResponse();
+    return;
+  }
+  alert("Barra de busqueda vacía");
 });
-
